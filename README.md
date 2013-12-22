47b9c730eefdeb9de311228cc57bad62d9931f91
Sagar G V <sagar.writeme@gmail.com>
2013-12-23 00:51:45 +0530

    Multi file reader and ZIP writer working

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ js-gerb-merge
 =============
 
 Merge gerbers and panelize PCBs in your browser. This project is on-going and not yet in a usable state.
+
+Libraries used
+--------------
+
+- jQuery 1.10.2
+- JSZip 2.0.0
---
+++ src/js-gerb-merge.js
@@ -0,0 +1,38 @@
+var zip = new JSZip();
+zip.file("hi.txt", "Hello world !!!\n");
+
+function fixDownload() {
+    var blobLink = document.getElementById('blob');
+    try {
+        blobLink.download = "hello.zip";
+        blobLink.href = window.URL.createObjectURL(zip.generate({type:"blob"}));
+    } catch(e) {
+        blobLink.innerHTML += " (not supported on this browser)";
+    }
+}
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        output.push('<li><strong>', escape(f.name), '</strong></li>');
+        var reader = new FileReader();
+        reader.onload = (function(theFile) {
+            return function(e) {
+                var bin = e.target.result;
+                zip.file(theFile.name,bin);
+                fixDownload();
+            }
+        })(f);
+        reader.readAsArrayBuffer(f);
+    }
+}
+
+window.onload = function () {
+    fixDownload();
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
+};
+
+

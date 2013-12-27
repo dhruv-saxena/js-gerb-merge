1df39347d3ff16969d9f8544b44c84f5feacd479
Sagar G V <sagar.writeme@gmail.com>
2013-12-28 01:02:40 +0530

    Improved scoping

--- src/js-gerb-merge.js
+++ src/js-gerb-merge.js
@@ -1,39 +1,39 @@
-var zip = new JSZip();
-zip.file("hi.txt", "Hello world !!!\n");
+(function() {
 
-function fixDownload() {
-    var blobLink = document.getElementById('blob');
-    try {
-        blobLink.download = "hello.zip";
-        blobLink.href = window.URL.createObjectURL(zip.generate({type:"blob"}));
-    } catch(e) {
-        blobLink.innerHTML += " (not supported on this browser)";
+    var zip = new JSZip();
+    zip.file("hi.txt", "Hello world !!!\n");
+
+    function fixDownload() {
+        var blobLink = document.getElementById('blob');
+        try {
+            blobLink.download = "hello.zip";
+            blobLink.href = window.URL.createObjectURL(zip.generate({type:"blob"}));
+        } catch(e) {
+            blobLink.innerHTML += " (not supported on this browser)";
+        }
+        return true;
     }
-    return true;
-}
 
-function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+    function handleFileSelect(evt) {
+        var files = evt.target.files; // FileList object
 
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-        output.push('<li><strong>', escape(f.name), '</strong></li>');
-        var reader = new FileReader();
-        reader.onload = (function(theFile) {
-            return function(e) {
-                var bin = e.target.result;
-                zip.file(theFile.name,bin);
-            }
-        })(f);
-        reader.readAsArrayBuffer(f);
+        // files is a FileList of File objects. List some properties.
+        for (var i = 0, f; f = files[i]; i++) {
+            var reader = new FileReader();
+            reader.onload = (function(theFile) {
+                return function(e) {
+                    var bin = e.target.result;
+                    zip.file(theFile.name,bin);
+                }
+            })(f);
+            reader.readAsArrayBuffer(f);
+        }
     }
-}
 
-window.onload = function () {
-    fixDownload();
-    document.getElementById('files').addEventListener('change', handleFileSelect, false);
-    document.getElementById('blob').onclick = fixDownload;
-};
+    window.onload = function () {
+        fixDownload();
+        document.getElementById('files').addEventListener('change', handleFileSelect, false);
+        document.getElementById('blob').onclick = fixDownload;
+    };
 
-
+}());

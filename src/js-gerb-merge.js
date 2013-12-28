371e2bbf6fd4d79b41cd9dc5eedfd274a49523d4
Sagar G V <sagar.writeme@gmail.com>
2013-12-28 19:47:44 +0530

    Callback on loading all gerbers

--- src/js-gerb-merge.js
+++ src/js-gerb-merge.js
@@ -1,5 +1,25 @@
 //(function() {
     var pcbs = [];
+    var paper;
+    
+    var start = function () {
+        this.startpath = this.attr("path").slice(0);
+        this.attr({stroke: '#000'});
+    };
+    var move = function (dx, dy) {
+        var path = this.attr("path")
+        for(var i=0; i < path.length - 1; i++) {
+            path[i][1] = this.startpath[i][1] + dx;
+            path[i][2] = this.startpath[i][2] + dy;
+        }
+        this.attr({path: path});
+    };
+    var up = function () {
+        this.attr({stroke: '#ddd'});
+    };
+
+    var addPCBtoUI = function(pcb) {
+    };
 
     function fixDownload() {
         var blobLink = document.getElementById('blob');
@@ -25,8 +45,17 @@
 
     function handleFileSelect(evt) {
         var files = evt.target.files; // FileList object
-        var pcb = new jspcb.PCB([]);
-        pcbs.push(pcb);
+        var nFilesRead = 0;
+        var gerbs = []; // array of ['filename.gtl','<content of filename.gtl>']
+        var pcbReadCallBack = function(gerber) {
+            nFilesRead += 1;
+            gerbs.push(gerber);
+            if(nFilesRead == files.length) {
+                // all gerbers read, now construct PCB
+                var pcb = new jspcb.PCB(gerbs);
+                pcbs.push(pcb);
+            }
+        };
         // files is a FileList of File objects. List some properties.
         for (var i = 0, f; f = files[i]; i++) {
             var reader = new FileReader();
@@ -35,7 +64,7 @@
                     var bin = e.target.result;
                     // The following code line assumes english only ASCII. To support other characters, different encoding is needed. 
                     var str = String.fromCharCode.apply(null, new Uint8Array(bin)); // convert array buffer to string
-                    pcb.addGerber([theFile.name, str]);
+                    pcbReadCallBack([theFile.name, str]);
                 }
             })(f);
             reader.readAsArrayBuffer(f);
@@ -46,42 +75,12 @@
         fixDownload();
         document.getElementById('files').addEventListener('change', handleFileSelect, false);
         document.getElementById('blob').onclick = fixDownload;
-        var paper = new Raphael(document.getElementById('canvas_container'),1920,1080);
 
-        var circle = paper.circle(100, 100, 25);
-        circle.attr({fill: '#9cf', stroke: '#ddd', 'stroke-width': 2});  
-        var start = function () {
-            this.ox = this.attr("cx");
-            this.oy = this.attr("cy");
-            this.attr({stroke: '#000'});
-        };
-        var move = function (dx, dy) {
-            this.attr({cx: this.ox + dx, cy: this.oy + dy});
-        };
-        var up = function () {
-            this.attr({stroke: '#ddd'});
-        };
-        circle.drag(move,start,up);
-        
-        var startp = function () {
-            this.startpath = this.attr("path").slice(0);
-            this.attr({stroke: '#000'});
-        };
-        var movep = function (dx, dy) {
-            var path = this.attr("path")
-            for(var i=0; i < path.length - 1; i++) {
-                path[i][1] = this.startpath[i][1] + dx;
-                path[i][2] = this.startpath[i][2] + dy;
-            }
-            this.attr({path: path});
-        };
-        var upp = function () {
-            this.attr({stroke: '#ddd'});
-        };
+        paper = new Raphael(document.getElementById('canvas_container'),1920,1080);
 
         polygon = paper.path('M 50 0 L 100 100 L 0 100 Z');
         polygon.attr({fill: '#9cf', stroke: '#ddd', 'stroke-width': 2});
-        polygon.drag(movep,startp,upp);
+        polygon.drag(move,start,up);
     };
 
 //}());
